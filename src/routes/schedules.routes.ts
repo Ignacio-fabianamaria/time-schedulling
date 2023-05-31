4589ae0e3b662c6a4e95268f03a84c5198e9a2cc
Siigmaf <faby.tbe@gmail.com>
2023-05-31 16:31:14 -0300

    feat:cria rota /POST para Schedules

--- src/routes/schedules.routes.ts
+++ src/routes/schedules.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
+import { SchedulesControlle } from "../controllers/SchedulesController";
 
 class SchedulesRoutes{
-    private router : Router;
+    private router: Router;
+    private schedulesController: SchedulesControlle;
+    
     constructor(){
-        this.router =Router()
+        this.router =Router();
+        this.schedulesController = new SchedulesControlle()
     }
     getRoutes():Router {
-        this.router.post('/');
+        this.router.post('/', this.schedulesController.store.bind(this.schedulesController));
         return this.router;
     }
 }
