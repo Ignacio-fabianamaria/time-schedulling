73f53a6dd6b3fd5aac940b1846a7a74a884ecdc7
Siigmaf <faby.tbe@gmail.com>
2023-06-01 18:58:15 -0300

    feat:cria pasta page e inicia a implementação da page Login e seu css

---
+++ frontend/src/page/Login/index.tsx
@@ -0,0 +1,7 @@
+import style from './Login.module.css';
+
+export function Login(){
+    return(
+        <div className={style.background}></div>
+    )
+}
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
+import { Login } from "../page/Login"
 
 export const RouteApp = ()=>{
     return(
         
         <Routes>
-            <Route path='/' element={
-            <>
-            <h1>Rota de Login</h1>
-            </>
-            }/>
+            <Route path='/' element={<Login />}/>
         </Routes>
        
     )
