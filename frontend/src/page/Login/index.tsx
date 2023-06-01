86a5867e8af73ed2e76a77c2d9f4294386992c5b
Siigmaf <faby.tbe@gmail.com>
2023-06-01 19:30:45 -0300

    feat:implementa conteudo inicial para tela de login

--- frontend/src/page/Login/index.tsx
+++ frontend/src/page/Login/index.tsx
@@ -1,7 +1,25 @@
 import style from './Login.module.css';
+import logo from '../../assets/logo.png';
 
-export function Login(){
-    return(
-        <div className={style.background}></div>
-    )
+export function Login() {
+  return (
+    <div className={style.background}>
+      <div className={`container ${style.container}`} >
+        <div className={style.wrapper}>
+          <div>
+            <img src={logo} alt='logo' />
+          </div>
+          <div className={style.card}>
+            <h2>Olá! Seja Bem Vindo.</h2>
+            <form>
+              <input type="text" />
+              <input type="text" />
+              <button></button>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
