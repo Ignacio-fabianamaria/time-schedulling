0ffcc32bf252be24cb231539901edc68db428101
Siigmaf <faby.tbe@gmail.com>
2023-06-02 15:27:52 -0300

    feat:Implementa validação de formulário utilizando react-hook-form e yup

--- frontend/src/components/input/index.tsx
+++ frontend/src/components/input/index.tsx
@@ -1,17 +1,24 @@
 import {AiOutlineMail} from 'react-icons/ai';
 import style from'./Input.module.css';
+import { ForwardRefRenderFunction, forwardRef } from 'react';
 
-interface IInput{placeholder?:string}
+interface IInput{
+    placeholder?:string;
+    type: 'password' | 'text' | 'date' ;
+    error?:string
+}
 
-export const Input = ({placeholder}:IInput)=>{
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, IInput>= ({placeholder, type, error, ...rest}, ref)=>{
     return(
         <div className={style.container} >
             <label htmlFor="">
-                <input type="text" placeholder={placeholder} ></input>
+                <input type={type} placeholder={placeholder} ref={ref} {...rest} ></input>
                 <i aria-hidden="true">
                     <AiOutlineMail size={25} />
                     </i>
             </label>
+            {error && <span>{error}</span>}
         </div>
     )
-}
+};
+export const Input = forwardRef(InputBase);
--- frontend/src/page/Login/index.tsx
+++ frontend/src/page/Login/index.tsx
@@ -1,8 +1,33 @@
 import style from './Login.module.css';
 import logo from '../../assets/logo.webp';
 import { Input } from '../../components/input';
+import {useForm} from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+
+interface IFormValues{
+  email:string;
+  password:string;
+}
 
 export function Login() {
+  const schema = yup.object().shape({
+    email: yup
+    .string()
+    .email('Digite um email válido')
+    .required('Campo de email obrigatório'),
+   password: yup
+   .string()
+   .min(6)
+   .required('Campo de senha origatório'),
+  });
+
+  const {register, handleSubmit, formState: {errors}} = useForm<IFormValues>({resolver: yupResolver(schema)});
+  
+  const submit = handleSubmit((data) => {
+    console.log("🚀 ~ file: index.tsx:28 ~ Login ~ data:", data)})
+
   return (
     <div className={style.background}>
       <div className={`container ${style.container}`} >
@@ -13,9 +38,18 @@ export function Login() {
           </div>
           <div className={style.card}>
             <h2>Olá! Seja Bem Vindo.</h2>
-            <form>
-             <Input placeholder="Email"/>
-             <Input placeholder="Senha" />
+            <form onSubmit={submit}>
+             <Input
+             placeholder="Email"
+             type = 'text'
+             error ={errors.email && errors.email.message}
+             {...register('email',{required:true})} 
+             />
+             <Input
+             placeholder="Senha"
+             type = 'password'
+             {...register('password',{required:true})} 
+             />
               <button></button>
             </form>
 
