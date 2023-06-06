c0c0c0ff2f23d2f2f8d609e9c3527222b000b6a6
Siigmaf <faby.tbe@gmail.com>
2023-06-06 13:41:01 -0300

    feat:cria tela de efitar perfil

--- frontend/src/page/editProfile/index.tsx
+++ frontend/src/page/editProfile/index.tsx
@@ -1,16 +1,36 @@
+import { useForm } from 'react-hook-form';
 import { Header } from '../../components/header';
 import { InputSchedule } from '../../components/inputSchedule';
 import style from './EditProfile.module.css';
 
-export function EditProfile(){
-    return(
-<div className='container'>
-    <Header />
-    <div className={style.formDiv}>
+interface IFormValues{
+  picture:File[];
+  name:string;
+  email:string;
+  password:string
+  newPassword:string;
+  oldPassword:string;
+}
+
+export function EditProfile() {
+  const {register, handleSubmit} = useForm<IFormValues>();
+  return (
+    <div className='container'>
+      <Header />
+      <div className={style.formDiv}>
         <form>
-            <InputSchedule placeholder='Name' type='text' />
+          <input type='file' {...register} />
+          <InputSchedule placeholder='Nome' type='text' />
+          <InputSchedule placeholder='Email' type='text' />
+          <InputSchedule placeholder='Senha Atual' type='password' />
+          <InputSchedule placeholder='Nova Senha' type='password' />
+          <InputSchedule placeholder='Confirmar nova senha' type='password' />
         </form>
+        <div className={style.footer}>
+          <button >Cancelar</button>
+          <button >Editar</button>
+        </div>
+      </div>
     </div>
-</div>
-    )
+  )
 }
